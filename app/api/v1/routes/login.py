d9d0ddbe164d6bbe9e026da6a00eea2538df7da9
lamido <uklamido@gmail.com>
2025-03-09 15:23:16 +0100

    fix: improved the logic for the login

--- app/api/v1/routes/login.py
+++ app/api/v1/routes/login.py
@@ -4,6 +4,7 @@ from app.api.v1.models.user_model import User
 from app.api.v1.utils.auth import verify_password
 from app.database import get_db
 from app.api.v1.schemas.user_schema import UserCreate
+from app.api.v1.services.crud import get_user_by_username
 
 router = APIRouter()
 
@@ -11,7 +12,7 @@ router = APIRouter()
 @router.post("/login")
 async def login(user: UserCreate, db: Session = Depends(get_db)):
     
-    db_user = db.query(User).filter(User.username == user.username).first()
+    db_user = get_user_by_username(db, user.username)
     if db_user is None:
         raise HTTPException(status_code=400, detail="Invalid username or password")
     
